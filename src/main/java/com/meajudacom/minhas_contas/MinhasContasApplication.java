4146e421ee084b088c5f2824f47902f845e1a377
Gustavo Paes <gustavo910@gmail.com>
2022-06-30 23:17:27 -0300

    first commit

---
+++ src/main/java/com/meajudacom/minhas_contas/MinhasContasApplication.java
@@ -0,0 +1,14 @@
+package com.meajudacom.minhas_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasContasApplication.class, args);
+		System.out.println("\nfoi né!?");
+	}
+	
+}
---
+++ src/test/java/com/meajudacom/minhas_contas/MinhasContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meajudacom.minhas_contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
